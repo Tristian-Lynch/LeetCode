1f2eccd8911ac78eb508decd2c3f70933967a8af
Tristian Lynch <96713488+Tristian-Lynch@users.noreply.github.com>
2024-10-21 13:37:31 -0400

    [2239] [Time Beats: 99.07%] [Memory Beats: 91.36%] - LeetPush

--- 2239-Find-Closest-Number-to-Zero.py
+++ 2239-Find-Closest-Number-to-Zero.py
@@ -3,16 +3,6 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        
-        The solution below gives us a time complexity of O(n).
-        For each item in the array we check to see if the abs()
-        of the current number and our current closest are equal
-        to catch an edge case of returning whichever value is greater
-        in this case.
-        Then if we do not encounter this edge case we just check to see if 
-        the absolute value of the current is less than or equal to the 
-        current closest. 
-        Returning our final closest value at the end.
         """
         closest = nums[0]
         for i in range(1,len(nums)):
