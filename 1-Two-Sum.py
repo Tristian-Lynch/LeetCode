2670a1ae1ce73d6c4a69873d9dfffdffa37b1ad4
Tristian Lynch <96713488+Tristian-Lynch@users.noreply.github.com>
2024-10-22 01:00:35 -0400

    [1] [Time Beats: 100.00%] [Memory Beats: 98.49%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -7,6 +7,12 @@ class Solution(object):
         \\\
         hash_map = {}
         i = 0
+        \\\
+        We iterate through the array adding vales to a hash_map, checking if the target minus
+        the current number is already in the hashmap, if so return true. 
+        Since looking up an item in a hash_map is O(n), 
+        we also have a O(n) time complexity.
+        \\\
         for x in nums:
             if target-x in hash_map:
                 return hash_map[target-x],i
