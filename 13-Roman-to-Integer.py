c4a65ce517993f27e16d31fa7971c301518d8d15
Tristian Lynch <96713488+Tristian-Lynch@users.noreply.github.com>
2024-10-22 00:46:59 -0400

    [13] [Time Beats: 99.46%] [Memory Beats: 41.05%] - LeetPush

--- 13-Roman-to-Integer.py
+++ 13-Roman-to-Integer.py
@@ -1,14 +1,31 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
+        \\\
         :type s: str
         :rtype: int
-        """
-        dict = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
-        result = dict[s[-1]]
-        for i in range(len(s) - 1):
-            if dict[s[i]] >= dict[s[i + 1]]:
-                result += dict[s[i]]
+        \\\
+        #Initialize dict of all roman numerals and their numeric value.
+        romans = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+
+        answer = 0
+        \\\
+        Iterate through the list of roman numerals, subtracting from the total if were
+        Not on the last number and the current numeral is less than the next.
+        Otherwise add the value of the numeral to the answer
+        This gives us a O(n) time complexity.
+        \\\
+        for i in range(len(s)):
+            if i < len(s)-1 and romans[s[i]] < romans[s[i+1]]:
+                answer -= romans[s[i]]
             else:
-                result -= dict[s[i]]
-        return result
+                answer += romans[s[i]]
+        
+        return answer
