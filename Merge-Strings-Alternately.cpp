eaf5ee942018eee87b3e3a098c4f0bcbc85396db
Tristian Lynch <96713488+Tristian-Lynch@users.noreply.github.com>
2025-06-11 20:25:45 -0400

    [1768] [Time Beats: 100.00%] [Memory Beats: 63.34%] - LeetPush

--- Merge-Strings-Alternately.cpp
+++ Merge-Strings-Alternately.cpp
@@ -3,16 +3,19 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        // where we will store our new word as its created.
         string newWord = "";
 
+        // two cases, one where the length of word1 is less than or equal to, and when word1 is greater than word2.
         if (word1.size() <= word2.size())
         {
+            // if word1 <= word2 size, then we iterate the length of word1 
             for (int i = 0; i < word1.size(); i++)
             {
                 newWord += word1[i];
                 newWord += word2[i];
             }
-
+            // and then append the rest of word two after all letters from word1 have been used.
             newWord += word2.substr(word1.size());
         }
         else if (word1.size() > word2.size())
@@ -25,6 +28,13 @@ public:
 
             newWord += word1.substr(word2.size());
         }
+
+        // finally return our new word.
         return newWord;
     }
 };
+
+// Good Time Complexity, O(M+N)
+// Variables werent edited in place so there is a space complexity of also O(M+N)
+
+// I could instead modify one of the existing variables in place adding a letter in every other spot from the other string.
